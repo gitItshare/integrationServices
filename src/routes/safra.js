c58a05d913fc097cc269987378695d902f2b27e1
Kevin Viscardi <viscardi.souza@gmail.com>
2024-04-02 11:53:08 -0300

    fix updated by

--- src/routes/safra.js
+++ src/routes/safra.js
@@ -729,7 +729,7 @@ router.post('/representantes',auth, async function(req, res) {
     const Params = req.body.Params
    const representantes = await safraServices.integration.consulta({Params}, token)
    console.log("TEKE", {data: resp})
-   res.json({data: representantes});
+   res.json({data: resp});
 });
 router.post('/templates',async function(req, res) {
     let resp = ""
