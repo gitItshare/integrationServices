0def2260ac4b81d53f25e92868778fbeda8f32dd
Leandro Boari <leandrobnsilva@gmail.com>
2024-04-05 11:18:18 -0300

    Correção safra

--- public/js/safra.js
+++ public/js/safra.js
@@ -1200,28 +1200,35 @@ function checkParameters3() {
 			}
 		}
 	})
-	statusComentTercElem = document.querySelectorAll("#statusComentTerc")
-	statusAvaElem = document.querySelectorAll("#statusAva")
-	statusComentAvaElem = document.querySelectorAll("#statusComentAva")
+	
 	statusComentAvaElem = document.querySelectorAll("#statusComentAva")
 	statusComentAvaMOElem = document.querySelectorAll("#statusComentAvaMO")
 	statusComentAvaMOElem.forEach((element, index) => {
 		if(!element.parentElement.hasAttribute("hidden")) {
 			console.log(element, index)
-			if(statusComentTercElem[index].value == "Status" || statusComentTercElem[index].value == "nao") {
-				var name = element.parentElement.parentElement.parentElement.parentElement.querySelector("legend.w-auto").innerText
-				name = name.trim()
-				commentElementsArr.push({
-					element, name
-				})
+
+			statusComentTercElem = statusComentAvaMOElem.parentElement.querySelectorAll("#statusComentTerc")
+			statusAvaElem = statusComentAvaMOElem.parentElement.querySelectorAll("#statusAva")
+
+			if(statusComentTercElem) {
+				if(statusComentTercElem[index].value == "Status" || statusComentTercElem[index].value == "nao") {
+					var name = element.parentElement.parentElement.parentElement.parentElement.querySelector("legend.w-auto").innerText
+					name = name.trim()
+					commentElementsArr.push({
+						element, name
+					})
+				}
 			}
-			if(statusAvaElem[index].value == "Status" || statusAvaElem[index].value == "nao") {
-				var name = element.parentElement.parentElement.parentElement.parentElement.querySelector("legend.w-auto").innerText
-				name = name.trim()
-				commentElementsArr.push({
-					element, name
-				})
+			if(statusAvaElem) {
+				if(statusAvaElem[index].value == "Status" || statusAvaElem[index].value == "nao") {
+					var name = element.parentElement.parentElement.parentElement.parentElement.querySelector("legend.w-auto").innerText
+					name = name.trim()
+					commentElementsArr.push({
+						element, name
+					})
+				}
 			}
+			
 		}
 	})
 	commentElementsArr.forEach((item, index) => {
