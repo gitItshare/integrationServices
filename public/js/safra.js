7d0278beb03cde85837ff0a1c5fde64e37fbd696
Leandro Boari <leandrobnsilva@gmail.com>
2024-04-05 13:47:34 -0300

    Correção erro contagem pessoas

--- public/js/safra.js
+++ public/js/safra.js
@@ -1078,15 +1078,13 @@ function checkParameters1() {
 
 	// Checagem se tem algum grupo
 	let countPeople = 0;
-	let terceiros = Array.from(document.getElementById("terceiros").children)
-	terceiros.forEach((el, index)=> {
+	Array.from(document.getElementById("terceiros").children).forEach((el, index)=> {
 		if(index > 0){
 			let terceirosContainer = Array.from(el.children[1].children[1].children)            
 			countPeople += terceirosContainer.length
 		}
 	})
-	let avalistas = Array.from(document.getElementById("avalistas").children)
-	avalistas.forEach((el, index)=> {
+	Array.from(document.getElementById("avalistas").children).forEach((el, index)=> {
 		if(index > 0){
 			let avalistaContainer = Array.from(el.children[1].children[1].children)
 			countPeople += avalistaContainer.length
